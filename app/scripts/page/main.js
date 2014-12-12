64275dc044af88fdf9124bfcefcf29b26d5dd2ea
aicekiller <1017634520@qq.com>
2014-12-12 15:02:23 +0800

    footer模板修改

--- app/scripts/page/main.js
+++ app/scripts/page/main.js
@@ -10,7 +10,9 @@ require(['can', 'sf.b2c.mall.header', 'sf.b2c.mall.footer'], function(can, heade
     },
 
     render: function() {
+
       new header('.sf-b2c-mall-header');
+      new footer('.sf-b2c-mall-footer');
     },
 
     supplement: function() {
