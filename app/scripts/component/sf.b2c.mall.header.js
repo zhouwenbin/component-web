cc1d6e5e7b96a7de833b22b2a101d64ea1ad2735
jnotnull <252210289@qq.com>
2014-12-12 12:52:22 +0800

    修改首页
    修改首页

--- app/scripts/component/sf.b2c.mall.header.js
+++ app/scripts/component/sf.b2c.mall.header.js
@@ -1,107 +1,109 @@
 'use strict';
 
-// @description 申明命名空间
-sf.util.namespace('b2c.mall.header');
-
 /**
- * @class sf.b2c.mall.header
- * @author Michael.Lee (leewind19841209@gmail.com)
- * @description 加载商品列表数据并且进行显示
+ * [description]
+ * @param  {[type]} can
+ * @return {[type]}
  */
-sf.b2c.mall.header = can.Control.extend({
+define('sf.b2c.mall.header', ['can'], function(can) {
 
-  defaults: {
-    login: {
-      my: 'center.html',
-      order: 'center.html#!&type=booking',
-      car: 'shopping.html'
+  can.Control.extend({
+
+    defaults: {
+      login: {
+        my: 'center.html',
+        order: 'center.html#!&type=booking',
+        car: 'shopping.html'
+      },
+      nologin: {
+        my: 'login.html',
+        order: 'login.html',
+        car: 'login.html'
+      }
     },
-    nologin: {
-      my: 'login.html',
-      order: 'login.html',
-      car: 'login.html'
+
+    /**
+     * @description 初始化方法，当调用new时会执行init方法
+     * @param  {Dom} element 当前dom元素
+     * @param  {Map} options 传递的参数
+     */
+    init: function(element, options) {
+      if ($.cookie('ct') == 1) {
+        this.data = new can.Map(_.extend(this.defaults.login, {
+          user: null
+        }));
+      } else {
+        this.data = new can.Map(_.extend(this.defaults.nologin, {
+          user: null
+        }));
+      }
+
+      this.render(this.data);
+      this.supplement();
+    },
+
+    /**
+     * @description 对页面进行渲染
+     * @param  {Map} data 渲染页面的数据
+     */
+    render: function(data) {debugger;
+      var html = can.view('templates/component/sf.b2c.mall.header.mustache', data);
+      this.element.html(html);
+    },
+
+    supplement: function() {
+      var that = this;
+      can.when(sf.b2c.mall.model.user.getUserInfo())
+        .done(function(data) {
+          if (sf.util.access(data)) {
+            that.data.attr('user', data.content[0]);
+
+            //解决IE789重复登录后无法访问到cookie问题
+            that.data.attr('my', that.defaults.login.my);
+            that.data.attr('order', that.defaults.login.order);
+            that.data.attr('car', that.defaults.login.car);
+          }
+        })
+        .fail(function(data) {})
+    },
+
+    '#user-logout click': function(element, event) {
+      event && event.preventDefault()
+
+      var that = this;
+      can.when(sf.b2c.mall.model.user.logout())
+        .done(function(data) {
+          if (sf.util.access(data) && data.content[0].value) {
+            that.data.attr('user', null);
+            window.localStorage.removeItem('csrfToken');
+            window.location.href = 'login.html'
+          };
+        })
+        .fail(function() {})
+    },
+
+    '#my-ht click': function(element, event) {
+      event && event.preventDefault();
+
+      if (sf.util.isLogin()) {
+        window.open('center.html')
+      } else {
+        window.open('login.html?from=center.html')
+      }
+
+      return false;
+    },
+
+    '#my-order click': function(element, event) {
+      event && event.preventDefault();
+
+      if (sf.util.isLogin()) {
+        window.open('center.html#!&type=booking')
+      } else {
+        window.open('login.html?from=' + encodeURIComponent('center.html#!&type=booking'));
+      }
+
+      return false;
     }
-  },
-
-  /**
-   * @description 初始化方法，当调用new时会执行init方法
-   * @param  {Dom} element 当前dom元素
-   * @param  {Map} options 传递的参数
-   */
-  init: function(element, options) {
-    if($.cookie('ct') == 1){
-      this.data = new can.Map(_.extend(this.defaults.login, {user: null}));
-    }else{
-      this.data = new can.Map(_.extend(this.defaults.nologin, {user: null}));
-    }
-
-    this.render(this.data);
-    this.supplement();
-  },
-
-  /**
-   * @description 对页面进行渲染
-   * @param  {Map} data 渲染页面的数据
-   */
-  render: function(data) {
-    var html = can.view('templates/component/sf.b2c.mall.header.mustache', data);
-    this.element.html(html);
-  },
-
-  supplement: function() {
-    var that = this;
-    can.when(sf.b2c.mall.model.user.getUserInfo())
-      .done(function(data) {
-        if (sf.util.access(data)) {
-          that.data.attr('user', data.content[0]);
-
-          //解决IE789重复登录后无法访问到cookie问题
-          that.data.attr('my', that.defaults.login.my);
-          that.data.attr('order', that.defaults.login.order);
-          that.data.attr('car', that.defaults.login.car);
-        }
-      })
-      .fail(function(data) {
-      })
-  },
-
-  '#user-logout click': function(element, event) {
-    event && event.preventDefault()
-
-    var that = this;
-    can.when(sf.b2c.mall.model.user.logout())
-      .done(function(data) {
-        if (sf.util.access(data) && data.content[0].value) {
-          that.data.attr('user', null);
-          window.localStorage.removeItem('csrfToken');
-          window.location.href = 'login.html'
-        };
-      })
-      .fail(function () {
-      })
-  },
-
-  '#my-ht click': function (element, event) {
-    event && event.preventDefault();
-
-    if (sf.util.isLogin()) {
-      window.open('center.html')
-    }else{
-      window.open('login.html?from=center.html')
-    }
-
-    return false;
-  },
-
-  '#my-order click': function (element, event) {
-    event && event.preventDefault();
-
-    if (sf.util.isLogin()) {
-      window.open('center.html#!&type=booking')
-    }else{
-      window.open('login.html?from='+encodeURIComponent('center.html#!&type=booking'));
-    }
-
-    return false;
-  }
+  });
 });
--- app/scripts/page/main.js
+++ app/scripts/page/main.js
@@ -1,44 +1,22 @@
 'use strict';
 
-sf.util.namespace('b2c.mall.launcher.home');
+define('b2c.mall.launcher.home', ['can'], function(can) {debugger;
 
-sf.b2c.mall.launcher.home = can.Control.extend({
+  var header = can.Control.extend({
 
-  init: function(element, options) {
-    this.render();
-    this.supplement();
-  },
+    init: function(element, options) {
+      this.render();
+      this.supplement();
+    },
 
-  render: function() {
-    new sf.b2c.mall.header('.sf-b2c-mall-header');
-    new sf.b2c.mall.footer('.sf-b2c-mall-footer');
+    render: function() {
+      new sf.b2c.mall.header('.sf-b2c-mall-header');
+    },
 
-    new sf.b2c.mall.component.nav.search('.sf-b2c-mall-nav');
+    supplement: function() {
 
-    can.when(sf.b2c.mall.model.adv.findSlidesAll())
-      .then(function(slides) {
+    }
+  });
 
-        new sf.b2c.mall.component.slide('.sf-b2c-mall-component-slide', {
-          data: slides
-        });
-
-        new sf.b2c.mall.banner.promise('.sf-b2c-mall-banner-promise');
-
-        new sf.b2c.mall.component.category('.sf-b2c-mall-component-category');
-
-      });
-  },
-
-  supplement: function () {
-      $(".content1-right").slide({
-          titCell:".hd ul",
-          mainCell:".bd ul",
-          autoPage:true,
-          effect:"left",
-          scroll:4,
-          vis:4
-      });
-  }
+  new header('#content');
 });
-
-new sf.b2c.mall.launcher.home('#content');
