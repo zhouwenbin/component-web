68f3c630f43b6db0d31ca012431b3f67c7a2ca34
Michael.Lee <leewind19841209@gmail.com>
2014-12-22 17:04:10 +0800

    [dev]首页重新打包

--- app/scripts/adapter/sf.b2c.mall.adapter.rapidSeaBuy.js
+++ app/scripts/adapter/sf.b2c.mall.adapter.rapidSeaBuy.js
@@ -1,25 +1,35 @@
 
 'use strict'
 
-define('sf.b2c.mall.adapter.rapidSeaBuy',[
-      'can',
-    ],function(can){
-      return new can.Map({
-        format:function(data){
-          var that = this;
+define(
+    'sf.b2c.mall.adapter.rapidSeaBuy',
 
-          _.each(data,function(value,key,list){
-            that.attr(key,value);
+    [
+      'can',
+      'underscore'
+    ],
+
+    function(can, _){
+      return can.Map({
+
+        format:function(data){
+          var arr = this.attr('fastSaleInfos');
+
+          _.each(data.fastSaleInfos, function(value, key, list){
+            arr.push(value)
           });
-          return that;
+
+          this.attr('pageInfo', data.pageInfo);
+          console.log(this)
         },
+
         formatPrice:function(data,priceData){
           _.each(data.fastSaleInfos,function(item){
-            _.each(priceData,function(priceItem){
-              if(item.homePageProductInfo && item.homePageProductInfo.itemId == priceItem.skuId){
+            _.each(priceData.value,function(priceItem){
+              if(item.homepageProductInfo && item.homepageProductInfo.itemId == priceItem.itemId){
                 item.attr('originPrice',priceItem.originPrice);
                 item.attr('sellingPrice',priceItem.sellingPrice);
-                item.attr('discount',priceItem.discount);
+                item.attr('discount', ((priceItem.sellingPrice/priceItem.originPrice)*10).toFixed(1));
               }else if(item.homepageTopicInfo){
                 item.attr('price', item.homepageTopicInfo.price);
                 item.attr('discount', item.homepageTopicInfo.discount);
