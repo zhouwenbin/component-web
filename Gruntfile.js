6f6e0729263d1d0b20801d674e7392154e4f5f86
Michael.Lee <leewind19841209@gmail.com>
2014-12-24 13:34:09 +0800

    [dev]add uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -492,7 +492,7 @@ module.exports = function (grunt) {
           'autoprefixer',
           'concat',
           'cssmin',
-          // 'uglify',
+          'uglify',
           'copy:dist',
           'requirejs:preheat',
           'requirejs:main',
