770c4a4948aa749ddd8bc93de7b0c0662cd509c5
Michael.Lee <leewind19841209@gmail.com>
2014-12-24 15:56:30 +0800

    [dev]加入detail的引导页

--- Gruntfile.js
+++ Gruntfile.js
@@ -445,6 +445,7 @@ module.exports = function (grunt) {
             'sf.b2c.mall.product.detailcontent',
             'vendor.jquery.imagezoom',
             'sf.b2c.mall.adapter.detailcontent',
+            'sf.b2c.mall.page.detail'
           ],
           insertRequire: ['sf.b2c.mall.page.detail']
         }
