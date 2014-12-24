9e7ae5adb18d8c91a84553e1b4c6fe7a22fe8ac0
jnotnull <252210289@qq.com>
2014-12-24 17:19:03 +0800

    [dev]

--- app/scripts/adapter/sf.b2c.mall.adapter.address.list.js
+++ app/scripts/adapter/sf.b2c.mall.adapter.address.list.js
@@ -1,36 +1,34 @@
 'use strict';
 
-// @description 申请命名空间
-sf.util.namespace('b2c.mall.adapter.address.list');
+define('b2c.mall.adapter.address.list', ['can'], function(can) {
 
-/**
- * @class sf.b2c.mall.adapter.address.list
- * @author Michael.Lee (leewind19841209@gmail.com)
- * @description CityList
- */
-sf.b2c.mall.adapter.address.list = can.Map.extend({
+  /**
+   * @class sf.b2c.mall.adapter.address.list
+   * @description CityList
+   */
+  return can.Map.extend({
 
-  init: function (data) {
-    this.attr('input', {
-      addrId: this.findDefaultAddress()
-    });
-  },
+    init: function(data) {
+      this.attr('input', {
+        addrId: this.findDefaultAddress()
+      });
+    },
 
-  findDefaultAddress: function () {
-    var address = this.addressList.attr('0');
-    return address && address.addrId;
-  },
+    findDefaultAddress: function() {
+      var address = this.addressList.attr('0');
+      return address && address.addrId;
+    },
 
-  remove: function (index) {
-    this.addressList.removeAttr(index);
-  },
+    remove: function(index) {
+      this.addressList.removeAttr(index);
+    },
 
-  get: function (index) {
-    return this.addressList.attr(index);
-  },
+    get: function(index) {
+      return this.addressList.attr(index);
+    },
 
-  length: function () {
-    return this.addressList.attr('length');
-  }
-
-});
+    length: function() {
+      return this.addressList.attr('length');
+    }
+  })
+})
--- app/scripts/order/sf.b2c.mall.order.selectreceiveperson.js
+++ app/scripts/order/sf.b2c.mall.order.selectreceiveperson.js
@@ -1,16 +1,58 @@
 'use strict';
 
-define('sf.b2c.mall.order.selectreceiveperson', ['can'], function(can) {
-  return can.Control.extend({
+define('sf.b2c.mall.order.selectreceiveperson', [
+    'can',
+    'md5',
+    'sf.b2c.mall.api.user.getIDCardUrlList',
+    'sf.b2c.mall.api.user.webLogin'
+  ],
+  function(can, md5, SFGetIDCardUrlList, SFUserWebLogin) {
+    return can.Control.extend({
 
-    /**
-     * 初始化
-     * @param  {DOM} element 容器element
-     * @param  {Object} options 传递的参数
-     */
-    init: function(element, options) {
-      var html = can.view('templates/order/sf.b2c.mall.order.selectrecperson.mustache')({});
-      this.element.html(html);
-    }
-  });
-})
+      defaults: {
+        user: new can.Map({
+          recId: null,
+          recName: null,
+          credtNum: null,
+          cellphone: null
+        })
+      }
+
+      /**
+       * 初始化
+       * @param  {DOM} element 容器element
+       * @param  {Object} options 传递的参数
+       */
+      init: function(element, options) {
+        this.render();
+      },
+
+      render: function() {
+        debugger;
+        var that = this;
+
+        //模拟登陆
+        var webLogin = new SFUserWebLogin({
+          accountId: 'jiyanliang@sf-express.com',
+          type: 'MAIL',
+          password: md5('123456' + 'www.sfht.com')
+        });
+
+        webLogin
+          .sendRequest()
+          .then(function() {
+            var getIDCardUrlList = new SFGetIDCardUrlList();
+            getIDCardUrlList
+              .sendRequest()
+              .done(function(data) {
+                debugger;
+
+                var html = can.view('templates/order/sf.b2c.mall.order.selectrecperson.mustache')({});
+                that.element.html(html);
+              })
+          })
+
+
+      }
+    });
+  })
