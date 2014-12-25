ac733c530440e8a208768f51c08802d31c1a71ab
aicekiller <1017634520@qq.com>
2014-12-25 21:23:50 +0800

    登录模块

--- app/scripts/page/sf.b2c.mall.page.login.js
+++ app/scripts/page/sf.b2c.mall.page.login.js
@@ -10,5 +10,46 @@ define(
   ],
 
   function ($, can, SFLogin) {
-    var login = new SFLogin('body');
+    var login = can.Control.extend({
+
+
+      init:function(){
+        this.component = {};
+        this.render();
+        this.supplement();
+      },
+
+      render:function(){
+        new SFLogin('body');
+      },
+      supplement:function(){
+        //----------关闭注册弹窗-------------//
+        $(".register .btn-close").on("click",function(){
+          $(this).parents(".register").hide(300);
+          $(".mask").hide();
+          return false;
+        });
+
+        //----------注册页小章鱼特效-------------//
+        $('.input-password').focus(function(){
+          $(".icon34").animate({
+            "top":-28
+          },700);
+        })
+        $('.input-password').blur(function(){
+          $(".icon34").animate({
+            "top":-78
+          },300)
+              .animate({
+                "top":-74
+              },50)
+              .animate({
+                "top":-76
+              },50);
+        })
+      }
+
+    });
+
+    new login('body');
   });
