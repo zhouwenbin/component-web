44dd500c837c04920d8b6bb97feb1b461b021c85
aicekiller <1017634520@qq.com>
2014-12-26 11:43:59 +0800

    bug fix

--- app/scripts/component/sf.b2c.mall.component.login.js
+++ app/scripts/component/sf.b2c.mall.component.login.js
@@ -191,14 +191,11 @@ define(
                 if (window.localStorage) {
                   window.localStorage.setItem('csrfToken', data.csrfToken)
                 } else {
-                  // 没什么用啊，写在hash上面
-                  // can.route.attr('csrfToken', data.content[0].csrfToken);
                   $.jStorage.set('csrfToken', data.csrfToken);
                 }
 
                 // deparam过程 -- 从url中获取需要请求的sku参数
                 var params = can.deparam(window.location.search.substr(1));
-
                 window.location.href = params.from || 'index.html';
               }
           })
