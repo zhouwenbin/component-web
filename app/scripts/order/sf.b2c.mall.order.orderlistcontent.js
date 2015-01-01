d092ad98594339d3278b3a7e6e7d11e0b01d304f
aicekiller <1017634520@qq.com>
2015-01-01 14:20:24 +0800

    【debug】【FRONT-311 订单list页面，商品多个规格展示需要分行】

--- app/scripts/order/sf.b2c.mall.order.orderlistcontent.js
+++ app/scripts/order/sf.b2c.mall.order.orderlistcontent.js
@@ -51,7 +51,8 @@ define('sf.b2c.mall.order.orderlistcontent', [
               if (order.orderGoodsItemList[0]) {
                 order.goodsName = order.orderGoodsItemList[0].goodsName;
                 order.imageUrl = order.orderGoodsItemList[0].imageUrl;
-                order.spec = order.orderGoodsItemList[0].spec;
+                order.spec = order.orderGoodsItemList[0].spec.split(',').join("<br/>");
+
                 order.optionHMTL = that.getOptionHTML(that.optionMap[order.orderStatus]);
                 order.showRouter = that.routeMap[order.orderStatus];
                 order.orderStatus = that.statsMap[order.orderStatus];
