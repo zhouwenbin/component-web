c3f076aa07fb36bba9a2c983f0b595508e00359b
wenbinzhou <330759294@qq.com>
2015-01-02 14:38:34 +0800

    幻灯修改

--- prototype/js/index.js
+++ prototype/js/index.js
@@ -18,21 +18,21 @@ $(function(){
 	//左右按钮hover
 	$(".slider").hover(function(){
 		sliderPrev.stop(true,false).animate({
-			left : "150px",
+			left : "0px",
 			opacity : 1
 		},500);
 		sliderNext.stop(true,false).animate({
-			right : "150px",
+			right : "0px",
 			opacity : 1
 		},500);
 		clearInterval(silderTimer);
 	},function(){
 		sliderPrev.stop(true,false).animate({
-			left : "50px",
+			left : "-50px",
 			opacity : 0
 		},500);
 		sliderNext.stop(true,false).animate({
-			right : "50px",
+			right : "-50px",
 			opacity : 0
 		},500);
 		silderTimer=setInterval(sliderNexting,5000);
