54f06690c0768cec22316061d894c0314d0e89aa
Michael.Lee <leewind19841209@gmail.com>
2015-01-02 11:15:42 +0800

    [dev]修复登录之后跳转的问题

--- app/scripts/component/sf.b2c.mall.component.header.js
+++ app/scripts/component/sf.b2c.mall.component.header.js
@@ -37,6 +37,8 @@ define('sf.b2c.mall.component.header', ['jquery',
       this.component = {};
       this.component.modal = new SFModal('body');
 
+      this.afterLoginDest = null;
+
       if (SFComm.prototype.checkUserLogin.call(this)) {
         this.data = new can.Map(_.extend(this.defaults.login, {
           isUserLogin: true,
@@ -76,9 +78,9 @@ define('sf.b2c.mall.component.header', ['jquery',
       event && event.preventDefault();
 
       if (SFComm.prototype.checkUserLogin.call(this)) {
-        window.location.pathname = SFConfig.setting.link.orderlist;
+        window.location.href = SFConfig.setting.link.orderlist;
       }else{
-        this.showLogin();
+        this.showLogin('orderlist');
       }
     },
 
@@ -86,9 +88,9 @@ define('sf.b2c.mall.component.header', ['jquery',
       event && event.preventDefault();
 
       if (SFComm.prototype.checkUserLogin.call(this)) {
-        window.location.pathname = SFConfig.setting.link.passwordchange;
+        window.location.href = SFConfig.setting.link.passwordchange;
       }else{
-        this.showLogin();
+        this.showLogin('passwordchange');
       }
     },
 
@@ -96,9 +98,9 @@ define('sf.b2c.mall.component.header', ['jquery',
       event && event.preventDefault();
 
       if (SFComm.prototype.checkUserLogin.call(this)) {
-        window.location.pathname = SFConfig.setting.link.center;
+        window.location.href = SFConfig.setting.link.center;
       }else{
-        this.showLogin();
+        this.showLogin('center');
       }
     },
 
@@ -134,7 +136,7 @@ define('sf.b2c.mall.component.header', ['jquery',
       if (SFComm.prototype.checkUserLogin.call(this)) {
 
       } else {
-        this.showLogin();
+        this.showLogin('center');
       }
     },
 
@@ -148,7 +150,11 @@ define('sf.b2c.mall.component.header', ['jquery',
       this.showRegister();
     },
 
-    showRegister: function () {
+    showRegister: function (dest) {
+      if (dest) {
+        this.afterLoginDest = dest
+      }
+
       this.component.modal.show({
         title: '登录顺丰海淘',
         html: '<iframe height="535px" width="100%" frameborder="no" seamless="" src="'+ SFConfig.setting.link.register +'">'
@@ -156,7 +162,11 @@ define('sf.b2c.mall.component.header', ['jquery',
       this.setIframe.call(this);
     },
 
-    showLogin: function() {
+    showLogin: function(dest) {
+      if (dest) {
+        this.afterLoginDest = dest
+      }
+
       this.component.modal.show({
         title: '登录顺丰海淘',
         html: '<iframe height="535px" width="100%" frameborder="no" seamless="" src="'+ SFConfig.setting.link.login +'">'
@@ -189,15 +199,25 @@ define('sf.b2c.mall.component.header', ['jquery',
       var that = this;
       // if (!this.component.modal.isClosed()) {
         setTimeout(function() {
+          if (that.component.modal.isClosed()) {
+            that.afterLoginDest = null
+          }
+
           console.log(SFComm.prototype.checkUserLogin.call(that))
           if (SFComm.prototype.checkUserLogin.call(that)) {
             that.component.modal.hide();
+
+            if (that.afterLoginDest) {
+              var link = SFConfig.setting.link[that.afterLoginDest];
+              window.location.href = link;
+            }
+
             that.watchLoginState.call(that);
             that.data.attr('isUserLogin', true);
           }else{
             that.data.attr('isUserLogin', false);
           }
-        }, 500);
+        }, 10000  );
       // }
     }
   });
