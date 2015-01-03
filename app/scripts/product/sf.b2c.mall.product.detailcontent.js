ed4a14bb574d76119f8e24afcf260759494633cd
jnotnull <252210289@qq.com>
2015-01-03 22:13:59 +0800

    [debug]更改购买按钮链接
    更改购买按钮链接

--- app/scripts/product/sf.b2c.mall.product.detailcontent.js
+++ app/scripts/product/sf.b2c.mall.product.detailcontent.js
@@ -304,7 +304,7 @@ define('sf.b2c.mall.product.detailcontent', [
       },
 
       '#gotobuy click': function() {
-        window.location.href = SFConfig.setting.link.detail + '?' + $.param({
+        window.location.href = 'http://www.sfht.com' + '?' + $.param({
           "itemId": $('.sf-b2c-mall-detail-content').eq(0).attr('data-itemid'),
           "saleid": $('.sf-b2c-mall-detail-content').eq(0).attr('data-saleid'),
           "amount": this.options.detailContentInfo.input.buyNum
