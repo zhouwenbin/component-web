ea8426a1254e87f5575237121a5d617976499a0e
jnotnull <252210289@qq.com>
2015-01-04 20:02:11 +0800

    [debug]FRONT-499详情页-推荐商品的价格未除以100
    FRONT-499详情页-推荐商品的价格未除以100

--- app/scripts/product/sf.b2c.mall.product.detailcontent.js
+++ app/scripts/product/sf.b2c.mall.product.detailcontent.js
@@ -171,8 +171,8 @@ define('sf.b2c.mall.product.detailcontent', [
           '<a class="fl" href="{{linkUrl}}"><img src="{{sf.img imageName}}" alt="" /></a>' +
           '<div class="recommend-c1">' +
           '<h3><a href="{{linkUrl}}">{{productName}}</a></h3>' +
-          '<div class="recommend-r1">¥{{sellingPrice}}</div>' +
-          '<div class="recommend-r2"><del>¥{{originPrice}}</del></div>' +
+          '<div class="recommend-r1">¥{{sf.price sellingPrice}}</div>' +
+          '<div class="recommend-r2"><del>¥{{sf.price originPrice}}</del></div>' +
           '</div>' +
           '</li>' +
           '{{/each}}' +
