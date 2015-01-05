96e4d78a12f5d00cfd548a69a89f4b20e16825fe
aicekiller <1017634520@qq.com>
2015-01-05 11:04:37 +0800

    [debug] FRONT-388 详情-缺少回到顶部的功能

--- app/scripts/page/sf.b2c.mall.page.detail.js
+++ app/scripts/page/sf.b2c.mall.page.detail.js
@@ -49,6 +49,19 @@ define(
 
       supplement: function() {
         // $('body').append('<iframe id="proxy" src="http://www.sfht.com/proxy.html" style="display:none;"></iframe>')
+        //----------回到顶部-------------//
+        $(window).scroll(function(){
+          if($(window).scrollTop() > 600){
+            $(".btn-top").fadeIn(500);
+          }else{
+            $(".btn-top").fadeOut(500);
+          }
+        })
+
+        $(".btn-top").click(function(){
+          $("body,html").animate({scrollTop:0},1000);
+          return false;
+        });
       }
     });
 
