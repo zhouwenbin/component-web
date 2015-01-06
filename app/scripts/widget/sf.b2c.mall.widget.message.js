f2be1b6fce95d6a6918ce55926ba471e56273bce
jnotnull <252210289@qq.com>
2015-01-06 17:35:30 +0800

    [dev]完善弹出组件
    完善弹出组件

--- app/scripts/widget/sf.b2c.mall.widget.message.js
+++ app/scripts/widget/sf.b2c.mall.widget.message.js
@@ -17,6 +17,7 @@ define(
         this.data.type = this.options.type;
         this.data.tip = this.options.tip;
         this.data.okFunction = this.options.okFunction;
+        this.data.closeFunction = this.options.closeFunction;
         this.data.buttons = this.buttonsMap[this.data.type];
 
         this.render();
@@ -29,7 +30,9 @@ define(
       },
 
       buttonsMap: {
-        'confirm': '<a href="javascript:void(0)" class="btn btn-send" id="ok">确定</a><a href="javascript:void(0)" class="btn btn-cancel" id="cancel">取消</a>'
+        'confirm': '<a href="javascript:void(0)" class="btn btn-send" id="ok">确定</a><a href="javascript:void(0)" class="btn btn-cancel" id="cancel">取消</a>',
+        'error': '<a href="javascript:void(0)" class="btn btn-send" id="ok">确定</a>',
+        'success': '<a href="javascript:void(0)" class="btn btn-send" id="ok">确定</a>'
       },
 
       '#close click': function() {
@@ -38,12 +41,17 @@ define(
       },
 
       '#ok click': function() {
-        this.data.okFunction.apply(this);
+        if (typeof this.data.okFunction != ''){
+          this.data.okFunction.apply(this);
+        }
         this.close();
         return false;
       },
 
       '#cancel click': function() {
+        if (typeof this.data.closeFunction != ''){
+          this.data.okFunction.apply(this);
+        }
         this.close();
         return false;
       },
