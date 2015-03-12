22f4abe247e544e8621b762fb48284266ee735c8
aicekiller <1017634520@qq.com>
2015-03-12 20:12:06 +0800

    嘿客收货地址bugfix

--- app/scripts/order/sf.b2c.mall.order.selectreceiveaddr.js
+++ app/scripts/order/sf.b2c.mall.order.selectreceiveaddr.js
@@ -87,7 +87,7 @@ define('sf.b2c.mall.order.selectreceiveaddr', [
           var fn = !_.isEmpty(params.orgCode) && map[params.saleid];
           var list = null
           if (_.isFunction(fn)) {
-            list = fn(reAddrs.items, data && data.value)
+            list = fn(that.result, data && data.value)
           }
 
           //获得地址列表
