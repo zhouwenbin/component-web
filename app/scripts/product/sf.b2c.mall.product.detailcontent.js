9b7fa7818e2cff9d5c51e66ace147df637dc4f97
aicekiller <1017634520@qq.com>
2015-03-25 18:18:08 +0800

    详情页价格渲染问题

--- app/scripts/product/sf.b2c.mall.product.detailcontent.js
+++ app/scripts/product/sf.b2c.mall.product.detailcontent.js
@@ -53,7 +53,17 @@ define('sf.b2c.mall.product.detailcontent', [
           } else {
             return options.inverse(options.contexts || this);
           }
+        },
+
+        //@todo 如果原价等于折扣价格，折扣价格不显示
+        'sf-is-originPrice':function(sellingPrice,originPrice) {
+          if (sellingPrice() == originPrice()) {
+            return options.fn(options.contexts || this);
+          } else {
+            return options.inverse(options.contexts || this);
+          }
         }
+
       },
 
       /**
@@ -621,13 +631,14 @@ define('sf.b2c.mall.product.detailcontent', [
 
       itemPriceTemplate: function() {
         return '<div class="goods-price-c1 fl">' +
-          '{{#if priceInfo.sellingPrice == priceInfo.originPrice}}'+
+          '{{#sf-is-originPrice  priceInfo.sellingPrice priceInfo.originPrice}}'+
             '<div class="goods-price-r1">促销价：<span>¥</span><strong>{{sf.price priceInfo.sellingPrice}}</strong></div>' +
             '国内参考价：￥155</div>' +
           '{{else}}'+
             '<div class="goods-price-r1">促销价：<span>¥</span><strong>{{sf.price priceInfo.sellingPrice}}</strong></div>' +
             '<div class="goods-price-r2">顺淘原价：￥{{sf.price priceInfo.originPrice}}   国内参考价：￥155</div>' +
-          '{{/if}}'+
+          '{{/}}'+
+
           '</div>' +
           '{{#sf-is-limitedTimeBuy priceInfo.productShape priceInfo.time}}' +
           '<div class="goods-price-c2">' +
