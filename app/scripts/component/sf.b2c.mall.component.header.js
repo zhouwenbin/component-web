472e7f1fa9abff3c83dadc984af849e16d44f96c
jnotnull <252210289@qq.com>
2015-04-16 12:56:06 +0800

    设置头部选中

--- app/scripts/component/sf.b2c.mall.component.header.js
+++ app/scripts/component/sf.b2c.mall.component.header.js
@@ -275,6 +275,8 @@ define('sf.b2c.mall.component.header', [
 
       };
 
+      that.setNavActive();
+
       // @note 通过服务端进行渲染，暂时这里不做动作
       // SFGetHeaderConfig
       //   .sendRequest()
@@ -288,6 +290,19 @@ define('sf.b2c.mall.component.header', [
       //   .fail(function (errorCode) {
 
       //   })
+    },
+
+    /**
+     * [setNavActive 导航设定为active]
+     */
+    setNavActive: function() {
+      var url = window.location.href;
+      var navHref = $(".nav-inner").find("a");
+      _.each(navHref, function(item){
+        if (item.href == url) {
+          $(item).parent().addClass("active");
+        }
+      })
     },
 
     /**
