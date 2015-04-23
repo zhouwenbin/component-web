87731923826f066e15bc0573017bbd5800899973
jnotnull <252210289@qq.com>
2015-04-23 20:02:22 +0800

    台湾原滋原味

--- app/scripts/page/sf.b2c.mall.page.naturalcoupon.js
+++ app/scripts/page/sf.b2c.mall.page.naturalcoupon.js
@@ -12,16 +12,13 @@ define("sf.b2c.mall.page.naturalcoupon", [
 
     SFFrameworkComm.register(1);
 
+    var header = new SFHeader('.sf-b2c-mall-header', {
+      "afterLoginDest": location.href
+    });
+
     var naturalcoupon = can.Control.extend({
 
       init: function(element, options) {
-
-        var header = new SFHeader('.sf-b2c-mall-header', {"afterLoginDest": location.href});
-
-        if (!SFFrameworkComm.prototype.checkUserLogin.call(this)) {
-          header.showLogin(location.href);
-          return false;
-        }
 
         this.options.data = new can.Map({});
 
@@ -29,6 +26,57 @@ define("sf.b2c.mall.page.naturalcoupon", [
       },
 
       render: function(element) {
+
+        if (!SFFrameworkComm.prototype.checkUserLogin.call(this)) {
+          this.options.data.attr("hasGetLift", false);
+          this.renderHtml(element, this.options.data);
+        } else {
+          var that = this;
+
+          var params = can.deparam(window.location.search.substr(1));
+          var bagid = params.bagid;
+
+          var receiveCoupon = new SFReceiveCoupon({
+            "type": "CARD", //"GIFTBAG",
+            "bagId": bagid,
+            "receiveChannel": 'B2C',
+            "receiveWay": 'ZTLQ'
+          });
+
+          receiveCoupon
+            .sendRequest()
+            .done(function(data) {
+              // 是否显示领取按钮，未领过还要显示领取按钮
+              that.options.data.attr("hasGetLift", true);
+              that.renderHtml(element, that.options.data);
+            })
+            .fail(function(error) {
+              // 已经领过了
+              if (error === 11000100) {
+                // 是否显示领取按钮 已经领过了就不显示了
+                that.options.data.attr("hasGetLift", true);
+                that.renderHtml(element, that.options.data);
+              } else {
+                console.error(error);
+              }
+            })
+        }
+
+      },
+
+      /** 渲染 */
+      renderHtml: function(element, data) {
+        var html = can.view('templates/natural/sf.b2c.mall.natural.coupon.mustache', data);
+        element.html(html);
+      },
+
+      "#getcoupon click": function(element, event) {
+
+        if (!SFFrameworkComm.prototype.checkUserLogin.call(this)) {
+          header.showLogin(location.href);
+          return false;
+        }
+
         var that = this;
 
         var params = can.deparam(window.location.search.substr(1));
@@ -45,32 +93,19 @@ define("sf.b2c.mall.page.naturalcoupon", [
           .sendRequest()
           .done(function(data) {
             // 是否显示领取按钮，未领过还要显示领取按钮
-            that.options.data.attr("showbutton", true);
-            that.options.data.attr("showcoupon", false);
+            that.options.data.attr("hasGetLift", true);
             that.renderHtml(element, that.options.data);
           })
           .fail(function(error) {
             // 已经领过了
             if (error === 11000100) {
               // 是否显示领取按钮 已经领过了就不显示了
-              that.options.data.attr("showbutton", false);
-              that.options.data.attr("showcoupon", true);
+              that.options.data.attr("hasGetLift", true);
               that.renderHtml(element, that.options.data);
             } else {
               console.error(error);
             }
           })
-      },
-
-      /** 渲染 */
-      renderHtml: function(element, data) {
-        var html = can.view('templates/natural/sf.b2c.mall.natural.coupon.mustache', data);
-        element.html(html);
-      },
-
-      "#getcoupon click": function(element, event) {
-        this.options.data.attr("showbutton", false);
-        this.options.data.attr("showcoupon", true);
       }
     });
 
