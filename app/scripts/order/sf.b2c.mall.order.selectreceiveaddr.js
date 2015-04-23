7e9034b852584f3fc2088225411e36336772a8b0
zhang.ke <zhang.ke@sf-express.com>
2015-04-23 15:36:19 +0800

    [活动一期]bug

--- app/scripts/order/sf.b2c.mall.order.selectreceiveaddr.js
+++ app/scripts/order/sf.b2c.mall.order.selectreceiveaddr.js
@@ -148,6 +148,10 @@ define('sf.b2c.mall.order.selectreceiveaddr', [
     },
 
     initItemInfo: function() {
+      //刷新订单信息时，先销毁之前的itemInfo （不销毁事件会重复绑定）
+      if (this.component.itemInfo) {
+        this.component.itemInfo.destroy();
+      }
       this.component.itemInfo = new SFItemInfo('.sf-b2c-mall-order-itemInfo', {
         vendorinfo: this.options.vendorinfo,
         selectReceiveAddr: this
