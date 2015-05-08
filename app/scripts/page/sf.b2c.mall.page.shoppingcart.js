24902915abed291ca176db3a531ea25ca11e5d23
aicekiller <1017634520@qq.com>
2015-05-08 13:48:11 +0800

    bug fix

---
+++ app/scripts/page/sf.b2c.mall.page.shoppingcart.js
@@ -0,0 +1,37 @@
+'use strict';
+
+define(
+  'sf.b2c.mall.page.shoppingcart',
+  [
+    'can',
+    'jquery',
+    'sf.b2c.mall.framework.comm',
+    'sf.util',
+    'sf.b2c.mall.business.config'
+  ],
+  function(can, $, SFFrameworkComm, SFFn, SFBusiness) {
+
+    SFFrameworkComm.register(1);
+    SFFn.monitor();
+    var findNoPage = can.Control.extend({
+
+      /**
+       * [init 初始化]
+       */
+      init: function() {
+        this.render();
+      },
+
+      /**
+       * [render 渲染]
+       */
+      render: function() {
+      	var that = this;
+
+        new SFRecommendProducts('.recommend');
+  		}
+
+    });
+
+    new findNoPage('#page-404');
+  })
