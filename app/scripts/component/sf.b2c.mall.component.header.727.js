11a0d855a066fa8eeafbf00aa44c2e85e5458294
jnotnull <252210289@qq.com>
2015-05-17 15:54:50 +0800

    增加广告

--- app/scripts/component/sf.b2c.mall.component.header.727.js
+++ app/scripts/component/sf.b2c.mall.component.header.727.js
@@ -53,7 +53,7 @@ define('sf.b2c.mall.component.header.727', [
     },
 
     ".banner-scroll click": function() {
-      window.location.href = "http://www.sfht.com/activity/553.html";
+      window.location.href = "http://www.sfht.com/activity/251.html?_src=media_vdsp";
     },
 
     isInShowPage: function() {
@@ -72,7 +72,7 @@ define('sf.b2c.mall.component.header.727', [
       }
 
       // 小鲜肉活动，如果是438页面则不显示
-      if (window.location.href.indexOf('553.html') > -1) {
+      if (window.location.href.indexOf('251.html') > -1) {
         return false;
       }
 
