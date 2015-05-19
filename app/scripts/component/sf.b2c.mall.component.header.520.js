dfadda23cfae9955c77587ac4a7a3eeba6543a5f
zhang.ke <zhang.ke@sf-express.com>
2015-05-19 19:51:17 +0800

    [520]金盾风控

--- app/scripts/component/sf.b2c.mall.component.header.520.js
+++ app/scripts/component/sf.b2c.mall.component.header.520.js
@@ -114,6 +114,30 @@ define('sf.b2c.mall.component.header.520', [
       var tag = can.route.attr('tag') || DEFAULT_FILLINFO_TAG;
 
       this.render(tag, this.data);
+
+      this.ininJinDun();
+    },
+
+    /**
+     * 调用金盾风控代码
+     */
+    ininJinDun: function() {
+      window._fmOpt = {
+        partner: 'sfht',
+        appName: 'sfht_web',
+        token: $.cookie("__da")
+      };
+      var cimg = new Image(1, 1);
+      cimg.onload = function() {
+        _fmOpt.imgLoaded = true;
+      };
+      cimg.src = "https://fp.fraudmetrix.cn/fp/clear.png?partnerCode=sfht&appName=sfht_web&tokenId=" + _fmOpt.token;
+      var fm = document.createElement('script');
+      fm.type = 'text/javascript';
+      fm.async = true;
+      fm.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'static.fraudmetrix.cn/fm.js?ver=0.1&t=' + (new Date().getTime() / 3600000).toFixed(0);
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(fm, s);
     },
 
     '{can.route} change': function() {
