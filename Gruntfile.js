d6590f4c5234d18c47d1ced89377a76254a71b65
aicekiller <1017634520@qq.com>
2015-05-19 18:29:27 +0800

    账户管理

--- Gruntfile.js
+++ Gruntfile.js
@@ -919,6 +919,45 @@ module.exports = function (grunt) {
         }
       },
 
+      shoppingcart: {
+        options: {
+          preserveLicenseComments: false,
+          baseUrl:          './app/',
+          out:            './<%= config.tmp %>/concat/scripts/sf.b2c.mall.page.shoppingcart.js',
+          mainConfigFile: "./<%= config.app %>/scripts/sf.b2c.mall.require.config.js",
+          paths: {
+            'placeholders':                 '../bower_components/Placeholders/dist/placeholders',
+            'moment':                       '../bower_components/momentjs/min/moment.min',
+            'moment-zh-cn':                 '../bower_components/momentjs/locale/zh-cn',
+            'text':                         '../bower_components/text/text',
+            'JSON':                         '../bower_components/JSON-js/json2',
+            'sf.b2c.mall.business.config':  'scripts/config/sf.b2c.mall.business.<%= config.target %>.config'
+          },
+          include:        ["JSON", "sf.b2c.mall.page.shoppingcart"],
+          insertRequire:  ['sf.b2c.mall.page.shoppingcart']
+        }
+      },
+      
+      addressmanage: {
+        options: {
+          preserveLicenseComments: false,
+          baseUrl:          './app/',
+          out:            './<%= config.tmp %>/concat/scripts/sf.b2c.mall.page.addressmanage.js',
+          mainConfigFile: "./<%= config.app %>/scripts/sf.b2c.mall.require.config.js",
+          paths: {
+            'placeholders':                 '../bower_components/Placeholders/dist/placeholders',
+            'moment':                       '../bower_components/momentjs/min/moment.min',
+            'moment-zh-cn':                 '../bower_components/momentjs/locale/zh-cn',
+            'text':                         '../bower_components/text/text',
+            'JSON':                         '../bower_components/JSON-js/json2',
+            'sf.b2c.mall.business.config':  'scripts/config/sf.b2c.mall.business.<%= config.target %>.config'
+          },
+          include:        ["JSON", "sf.b2c.mall.page.addressmanage"],
+          insertRequire:  ['sf.b2c.mall.page.addressmanage']
+        }
+      },
+
+
       activityend: {
         options: {
           preserveLicenseComments: false,
