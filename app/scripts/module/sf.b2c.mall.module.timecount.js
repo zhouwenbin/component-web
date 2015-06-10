a5401c214a1d0e35c56fa6aa649dd876f2ca779a
aicekiller <1017634520@qq.com>
2015-06-10 14:50:52 +0800

    倒计时组件

--- app/scripts/module/sf.b2c.mall.module.timecount.js
+++ app/scripts/module/sf.b2c.mall.module.timecount.js
@@ -1,3 +1,5 @@
+'use strict';
+
 define(
   'sf.b2c.mall.module.timecount', [
     'can',
@@ -12,52 +14,53 @@ define(
     var deadTime = can.Control.extend({
 
       init: function(element, options) {
-       //  setInterval(this.refeshTime(element), 1000);
-          this.refeshTime(element);
+        this.refeshTime(element);
       },
-        refeshTime: function(element){
-            var textValue = this.timeCount(element.attr("time-line"));
-            element.text(textValue);
-        },
 
-       //deadTimeLine的时间格式为20150609211912，意思是2015年6月9日21点19分12秒
-        timeCount: function (deadTimeLine){
-          var yearValue = deadTimeLine.substring(0,4);
-          var monthValue = deadTimeLine.substring(4,6)
-          var dayValue = deadTimeLine.substring(6,8)
-          var hourValue = deadTimeLine.substring(8,10)
-          var MinValue = deadTimeLine.substring(10,12)
-          var SecValue = deadTimeLine.substring(12,14)
-          var endDate=new Date(yearValue,monthValue-1,dayValue,hourValue,MinValue,SecValue);//年月日时分秒，月要减去1
-          var now=new Date();
-          var oft=Math.round((endDate-now)/1000);
-          var ofd=parseInt(oft/3600/24);
-          var ofh=parseInt((oft%(3600*24))/3600);
-          var ofm=parseInt((oft%3600)/60);
-          var ofs=oft%60;
-          var timeInterval = '还有 ';
-          if(ofd > 0){
-              timeInterval += ofd+' 天 ';
-          }
+      //
+      refeshTime: function(element) {
+        var textValue = this.timeCount(element.attr("time-line"));
+        element.text(textValue);
+      },
 
-          if(ofs<0){
-              timeInterval = "开抢";
-              return timeInterval;
-          }
+      //deadTimeLine的时间格式为20150609211912，意思是2015年6月9日21点19分12秒
+      timeCount: function(deadTimeLine) {
+        var yearValue = deadTimeLine.substring(0, 4);
+        var monthValue = deadTimeLine.substring(4, 6)
+        var dayValue = deadTimeLine.substring(6, 8)
+        var hourValue = deadTimeLine.substring(8, 10)
+        var MinValue = deadTimeLine.substring(10, 12)
+        var SecValue = deadTimeLine.substring(12, 14)
+        var endDate = new Date(yearValue, monthValue - 1, dayValue, hourValue, MinValue, SecValue); //年月日时分秒，月要减去1
+        var now = new Date();
+        var oft = Math.round((endDate - now) / 1000);
+        var ofd = parseInt(oft / 3600 / 24);
+        var ofh = parseInt((oft % (3600 * 24)) / 3600);
+        var ofm = parseInt((oft % 3600) / 60);
+        var ofs = oft % 60;
+        var timeInterval = '还有 ';
+        if (ofd > 0) {
+          timeInterval += ofd + ' 天 ';
+        }
 
-          timeInterval = timeInterval +ofh+ ' 小时 ' +ofm+ ' 分钟 ' +ofs+ ' 秒';
+        if (ofs < 0) {
+          timeInterval = "开抢";
           return timeInterval;
+        }
+
+        timeInterval = timeInterval + ofh + ' 小时 ' + ofm + ' 分钟 ' + ofs + ' 秒';
+        return timeInterval;
       }
     })
 
     // 查到所有需要倒计时的模块
     var timeModules = $('.timer');
 
-    // 分别进行实例化
 
-    setInterval(function(){
-        _.each(timeModules, function(timeModule) {
-            new deadTime($(timeModule));
-        });
-    },1000);
+    // 分别进行实例化 
+    setInterval(function() {
+      _.each(timeModules, function(timeModule) {
+        new deadTime($(timeModule));
+      });
+    }, 1000);
   })
