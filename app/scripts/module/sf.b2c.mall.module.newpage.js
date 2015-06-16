41af58855e188c84a23398d6d7fa267130e93e77
sfyangkai <kaiyang1@sf-express.com>
2015-06-16 20:21:36 +0800

    增加温馨提示

---
+++ app/scripts/module/sf.b2c.mall.module.newpage.js
@@ -0,0 +1,58 @@
+/**
+ * Created by 魏志强 on 2014/12/26.
+ */
+define(
+    'sf.b2c.mall.module.slider', [
+        'can',
+        'jquery',
+        'underscore',
+        'sf.b2c.mall.widget.slide'
+    ],
+    function(can, $, _, SFSlide) {
+
+        var main = can.Control.extend({
+
+            init: function(element) {
+                this.render(element);
+
+
+            },
+            // renderMap: {
+            //   'slide': function () {
+            //     var $el = this.element.find('.sf-b2c-mall-main-slider.serverRendered');
+
+            //     var that = this;
+            //     if ($el.length === 0) {
+            //       var request = new SFApiGetBanner();
+            //       can.when(request.sendRequest())
+            //         .done(function (data) {
+            //           var arr = [];
+            //           _.each(data.value, function(value, key, list){
+            //             arr.push({
+            //               imgUrl: value.imgUrl,
+            //               url: value.link
+            //             });
+            //           });
+            //           that.component.slide = new SFSlide('.sf-b2c-mall-main-slider', {imgs: arr});
+            //         })
+            //         .fail(function () {
+
+            //         })
+            //     }else{
+            //       that.component.slide = new SFSlide('.sf-b2c-mall-main-slider');
+            //     }
+            //   }
+            // },
+
+            render: function(element) {
+                new SFSlide(element);
+            }
+        })
+
+
+        //一个界面支持多个轮播组件
+        var sliderModules = $(".cms-module-slider");
+        _.each(sliderModules, function(sliderModule) {
+            new main($(sliderModule));
+        });
+    })
