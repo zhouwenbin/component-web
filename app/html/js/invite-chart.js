e1a9e2070c3814045bcd2dbceb7645651dfc0fe1
user <user@yiyangdeMacBook-Pro.local>
2015-06-28 17:56:50 +0800

    老带新

--- app/html/js/invite-chart.js
+++ app/html/js/invite-chart.js
@@ -1,61 +1,46 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+  window.onload = function () {
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
+      theme: "theme4",
+      animationEnabled: true,
+      axisX: {
+        valueFormatString: "MM.DD",
+        interval:1,
+        intervalType: "day",
+        lineColor:"#50E3C2",
+        lineDashType:"dash",
+        labelFontColor:"#50E3C2",
+        tickColor:"#50E3C2",
+        tickThickness: 2
+        
+      },
+      axisY:{
+        includeZero: false,
+        lineColor:"#fff",
+        labelFontColor:"#fff",
+        tickColor:"#fff"
+        
+      },
+      data: [
+      {        
+        type: "line",
+        lineThickness: 4,
+        color: "rgba(255,158,54,0.5)",    
+        dataPoints: [
+        { x: new Date(2015, 05, 19), y: 600 , indexLabel: "600",indexLabelFontColor:"#FF9E36"},
+        { x: new Date(2015, 05, 20), y: 1000 , indexLabel: "1000",indexLabelFontColor:"#FF9E36" },
+        { x: new Date(2015, 05, 21), y: 800 , indexLabel: "800",indexLabelFontColor:"#FF9E36" },
+        { x: new Date(2015, 05, 22), y: 200 , indexLabel: "200",indexLabelFontColor:"#FF9E36" },
+        { x: new Date(2015, 05, 23), y: 900 , indexLabel: "900",indexLabelFontColor:"#FF9E36" },
+        { x: new Date(2015, 05, 24), y: 700 , indexLabel: "700",indexLabelFontColor:"#FF9E36" },
+        { x: new Date(2015, 05, 25), y: 1000 , indexLabel: "1000",indexLabelFontColor:"#FF9E36" }
+        
+        ]
+      }
+      
+      
+      ]
+    });
 
-var parseDate = d3.time.format("%d-%b-%y").parse;
-
-var x = d3.time.scale()
-    .range([0, width]);
-
-var y = d3.scale.linear()
-    .range([height, 0]);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-
-var line = d3.svg.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.close); });
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-d3.tsv("data.tsv", function(error, data) {
-  if (error) throw error;
-
-  data.forEach(function(d) {
-    d.date = parseDate(d.date);
-    d.close = +d.close;
-  });
-
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain(d3.extent(data, function(d) { return d.close; }));
-
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Price ($)");
-
-  svg.append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("d", line);
-});
+chart.render();
+}
