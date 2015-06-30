d7a002b54c013ac1cfbe68efa3ef2ea6ffb7aa7e
jnotnull <252210289@qq.com>
2015-06-30 19:57:05 +0800

    老带新落地页

--- app/scripts/page/sf.b2c.mall.page.invitationshare.js
+++ app/scripts/page/sf.b2c.mall.page.invitationshare.js
@@ -1,23 +1,32 @@
 'use strict';
 
 define(
-  'sf.b2c.mall.page.invitation', [
+  'sf.b2c.mall.page.invitationshare', [
     'can',
     'jquery',
     'sf.b2c.mall.framework.comm',
     'sf.b2c.mall.component.header',
     'sf.b2c.mall.component.footer',
-    'sf.b2c.mall.component.centerleftside',
-    'sf.b2c.mall.center.invitationcontent',
     'sf.b2c.mall.business.config',
-    'template_center_invitationshare'
+    'text!template_center_invitationshare',
+    'sf.b2c.mall.api.user.getUserInfo'
   ],
 
-  function(can, $, SFFrameworkComm, Header, Footer, Centerleftside, SFInvitationcontent, SFBusiness, template_center_invitationshare) {
+  function(can, $, SFFrameworkComm, Header, Footer, SFBusiness, template_center_invitationshare, SFGetUserInfo) {
 
     SFFrameworkComm.register(1);
 
     var orderList = can.Control.extend({
+
+      helpers: {
+        isLogin: function(options) {
+          if (SFFrameworkComm.prototype.checkUserLogin.call(this)) {
+            return options.fn(options.contexts || this);
+          } else {
+            return options.inverse(options.contexts || this);
+          }
+        }
+      },
 
       /**
        * [init 初始化]
@@ -32,15 +41,56 @@ define(
        * [render 执行渲染]
        */
       render: function() {
-        var header = new Header('.sf-b2c-mall-header', {
+        this.header = new Header('.sf-b2c-mall-header', {
           channel: '首页',
           isForceLogin: true
         });
         new Footer('.sf-b2c-mall-footer');
 
         var renderFn = can.mustache(template_center_invitationshare);
-        this.options.html = renderFn(this.data);
+        this.options.html = renderFn(this.data, this.helpers);
         this.element.html(this.options.html);
+
+        // this.supplement();
+
+
+        if (!SFFrameworkComm.prototype.checkUserLogin.call(this)) {
+          // this.header.showLogin();
+        }
+      },
+
+      supplement: function() {
+        if (SFFrameworkComm.prototype.checkUserLogin.call(this)) {
+          this.renderQrcode();
+        }
+      },
+
+      /** 渲染二维码 */
+      renderQrcode: function() {
+        var getUserInfo = new SFGetUserInfo();
+        getUserInfo.sendRequest()
+          .done(function(userinfo) {
+            var url = "http://www.sfht.com?_src=" + userinfo.userId;
+            var qrParam = {
+              width: 125,
+              height: 125,
+              text: url
+            };
+
+            $('#shareURLQrcode').html("").qrcode(qrParam);
+            $('#urlinput').val(url);
+          })
+          .fail()
+      },
+
+      "#logintoShare click": function(element, event) {
+        event && event.preventDefault();
+
+        if (!SFFrameworkComm.prototype.checkUserLogin.call(this)) {
+          this.header.showLogin();
+          return false;
+        }
+
       }
     });
 
