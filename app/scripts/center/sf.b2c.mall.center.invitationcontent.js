21216d62812acc54b60a93f75adad1c455af42f2
jnotnull <252210289@qq.com>
2015-07-02 17:49:43 +0800

    修改bug

--- app/scripts/center/sf.b2c.mall.center.invitationcontent.js
+++ app/scripts/center/sf.b2c.mall.center.invitationcontent.js
@@ -54,12 +54,6 @@ define('sf.b2c.mall.center.invitationcontent', [
         can.when(getCashActInfo.sendRequest(), getCashActTransList.sendRequest())
           .done(function(mainInfo, infoList) {
 
-            var mainInfo = {
-              "bindAliAct": null,
-              "lastestIncome": "200",
-              "totalIncome": 400,
-              "actBalance": "10000"
-            }
             var infoList = {
               "infos": [{
                 "income": 100,
