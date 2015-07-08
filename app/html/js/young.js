7809a461d9de2a9734e5935f41558577ee00f087
user <user@yiyangdeMacBook-Pro.local>
2015-07-08 19:09:05 +0800

    小鲜肉

--- app/html/js/young.js
+++ app/html/js/young.js
@@ -1,10 +1,10 @@
 $(function(){
   $( '#st-stack' ).stackslider();
 
-  
+
 	$('.young-tab-h li').click(function(){
     var index = $('.young-tab-h li').index(this);
     $(this).addClass('active').siblings().removeClass('active');
-    $('.young-tab-b li').eq(index).addClass('active').siblings().removeClass('active');
+    $('.young-tab-b>li').eq(index).addClass('active').siblings().removeClass('active');
   })
 })
