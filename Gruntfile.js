1366b112db9d6333e9bc1d20bb2eeb4f3cfaf770
sfyangkai <kaiyang1@sf-express.com>
2015-07-09 16:56:26 +0800

    build package

--- Gruntfile.js
+++ Gruntfile.js
@@ -988,6 +988,25 @@ module.exports = function(grunt) {
         }
       },
 
+        young: {
+            options: {
+                preserveLicenseComments: false,
+                baseUrl: './app/',
+                out: './<%= config.tmp %>/concat/scripts/sf.b2c.mall.page.young.js',
+                mainConfigFile: "./<%= config.app %>/scripts/sf.b2c.mall.require.config.js",
+                paths: {
+                    'placeholders': '../bower_components/Placeholders/dist/placeholders',
+                    'moment': '../bower_components/momentjs/min/moment.min',
+                    'moment-zh-cn': '../bower_components/momentjs/locale/zh-cn',
+                    'text': '../bower_components/text/text',
+                    'JSON': '../bower_components/JSON-js/json2',
+                    'sf.b2c.mall.business.config': 'scripts/config/sf.b2c.mall.business.<%= config.target %>.config'
+                },
+                include: ["JSON", "sf.b2c.mall.page.young"],
+                insertRequire: ['sf.b2c.mall.page.young']
+            }
+        },
+
         integralactive: {
             options: {
                 preserveLicenseComments: false,
