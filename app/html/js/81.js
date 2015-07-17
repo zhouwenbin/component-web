bd5466c699eea0f71336f14722ddf5067202b67e
user <user@yiyangdeMacBook-Pro.local>
2015-07-17 11:55:07 +0800

    视频效果

--- app/html/js/81.js
+++ app/html/js/81.js
@@ -10,6 +10,19 @@ $(function(){
 			top:0
 		})
 	});
-	
   }
+  //时间倒计时
+  var target_day=27;
+  var date = new Date();
+  var current_day=date.getDate();
+  var last_day=target_day-current_day;
+  $('.m81-day').text(last_day);
+  //播放视频
+  $('.m81-play').click(function(){
+  	$('.m81-video').show();
+  	return false;
+  })
+  $('html').click(function(){
+  	$('.m81-video').hide();
+  })
 })
