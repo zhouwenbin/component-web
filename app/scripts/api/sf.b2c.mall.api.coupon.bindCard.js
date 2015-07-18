df6968da8e93ec159f7edde52ceba40e624c0349
honey peach <wangxiafei@sf-express.com>
2015-07-18 13:21:57 +0800

    717

---
+++ app/scripts/api/sf.b2c.mall.api.coupon.bindCard.js
@@ -0,0 +1,43 @@
+// Auto Generated.  DO NOT EDIT!
+/**
+  * @class sf.b2c.mall.api.coupon.bindCard
+  * @param  {Object} $
+  * @param  {Object} can
+  * @param  {Object} _
+  * @param  {can.Construct} Comm
+  * @param  {Object} SecurityType
+  * @return {can.Construct}
+  */
+define(
+'sf.b2c.mall.api.coupon.bindCard',
+[
+  'jquery',
+  'can',
+  'underscore',
+  'sf.b2c.mall.framework.comm',
+  'sf.b2c.mall.api.security.type'
+],
+function($, can, _, Comm, SecurityType) {
+  'use strict';
+
+  return Comm.extend({
+    api: {
+      METHOD_NAME: 'coupon.bindCard',
+      SECURITY_TYPE: SecurityType.None.name,
+      REQUIRED: {
+        'name': 'string',
+        'mobile': 'string',
+        'ids': 'string'
+      },
+      OPTIONAL: {
+      },
+      VERIFY:{
+      },
+      ERROR_CODE: {
+        '11000240': '用户今天的券已经领完了',
+        '11000250': '用户已经领完该活动期间所有的券',
+        '11000260': '用户输入手机号有误'
+      }
+    }
+  });
+});
