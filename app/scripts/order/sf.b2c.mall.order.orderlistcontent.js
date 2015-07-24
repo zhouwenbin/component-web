61b8cbf9341b3a60d73f2b175c959dcc31c14130
aicekiller <1017634520@qq.com>
2015-07-24 15:35:30 +0800

    去掉设为默认收货人操作

--- app/scripts/order/sf.b2c.mall.order.orderlistcontent.js
+++ app/scripts/order/sf.b2c.mall.order.orderlistcontent.js
@@ -272,12 +272,7 @@ define('sf.b2c.mall.order.orderlistcontent', [
                     that.options.orders[i].leftTime -= 1000;
                   }
 
-                  // if (that.options.orders[i] && that.options.orders[i].gmtEnd > 0 && endTimeArea.eq(i)) {
-                  //     item.leftTime = that.options.orders[i].gmtEnd - new Date().getTime() - DIFF;
-                  //     that.setCountDown(endTimeArea.eq(i), item.leftTime);
-                  //     item.leftTime -= 1000;
-                  //     //console.log(item.leftTime);
-                  // }
+
 
                 });
 
@@ -368,21 +363,13 @@ define('sf.b2c.mall.order.orderlistcontent', [
       },
       //倒计时
       setCountDown: function(element, leftTime) {
-        if (leftTime <= 0) {
-          window.location.reload();
-          return false;
-        }
+
         var leftsecond = parseInt(leftTime / 1000);
         var day1 = Math.floor(leftsecond / (60 * 60 * 24));
         var hour = Math.floor((leftsecond - day1 * 24 * 60 * 60) / 3600);
         var minute = Math.floor((leftsecond - day1 * 24 * 60 * 60 - hour * 3600) / 60);
         var second = Math.floor(leftsecond - day1 * 24 * 60 * 60 - hour * 3600 - minute * 60);
         $(element).html(hour+ "小时" +minute + "分" + second + "秒");
-        // if (hour <= 0) {
-        //   $(element).html(minute + "分" + second + "秒");
-        // }else{
-        //   $(element).html(hour+ "小时" +minute + "分" + second + "秒");
-        // }
 
       },
 
