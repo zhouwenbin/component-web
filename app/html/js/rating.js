603f4c80cb605d9ce82b37ce413b100782b0bf44
zhouwenbin <330759294@qq.com>
2015-08-05 16:20:01 +0800

    add

---
+++ app/html/js/rating.js
@@ -0,0 +1,14 @@
+$(function(){
+  //评分
+  $.fn.raty.defaults.path = 'lib/raty/lib/images';
+  $('.rating').raty({ score: 5 });
+  //切换
+  $('.detail-tab-h li').click(function(){
+    var index=$('.detail-tab-h li').index(this);
+    $(this).addClass('active').siblings().removeClass('active');
+    $('.detail-tab-b').eq(index).addClass('active').siblings().removeClass('active');
+  })
+  $('.comment-tab li').click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+  })
+})
