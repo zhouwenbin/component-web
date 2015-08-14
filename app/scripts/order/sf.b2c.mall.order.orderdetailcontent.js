b093c81506fdcc5b5e3faff828c6a39296072b5b
aicekiller <1017634520@qq.com>
2015-08-14 11:59:03 +0800

    bug fix

--- app/scripts/order/sf.b2c.mall.order.orderdetailcontent.js
+++ app/scripts/order/sf.b2c.mall.order.orderdetailcontent.js
@@ -56,6 +56,18 @@ define('sf.b2c.mall.order.orderdetailcontent', [
           if (refundTax.state == stateText) {
             return options.fn(options.contexts || this);
           }
+        },
+        'sf-show-refundTax': function(refundTax, options) {
+          if (typeof refundTax.state !== 'undefined') {
+            return options.fn(options.contexts || this);
+          } else {
+            return options.inverse(options.contexts || this);
+          }
+        },
+        'isShowRefundTax': function(status, options) {
+          if (status == "CONSIGNED" || status == 'COMPLETED' || status == 'AUTO_COMPLETED' || status == 'RECEIPTED') {
+            return options.fn(options.contexts || this);
+          }
         }
       },
       init: function(element, options) {
