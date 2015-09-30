2bb5b0be96d866086f77899f81839633eef2fe5a
Michael.Lee <leewind19841209@gmail.com>
2015-09-30 17:41:41 +0800

    [dev] 国庆期间放假通知－－只在首页详情页活动页展示的通知信息

--- app/scripts/component/sf.b2c.mall.component.header.js
+++ app/scripts/component/sf.b2c.mall.component.header.js
@@ -193,15 +193,20 @@ define('sf.b2c.mall.component.header', [
         // this.renderMap['template_header_727'].call(this, that.data);
 
         // 全站通告不受限制
-        var isNotShowNotice = false;
         var pathname = window.location.pathname;
         // var isNotShowNotice = (pathname == '/') || (pathname.indexOf('index.html') > -1) || (pathname.indexOf('activity') > -1)
         var isNotShowNotice = (pathname.indexOf('orderlist.html') == -1);
 
+        var isShowOtherNotice = (pathname == '/') || (pathname.indexOf('index.html') > -1) || (pathname.indexOf('activity') > -1) || (pathname.indexOf('detail') > -1)
+
         if (!isNotShowNotice) {
           $('.notice .pm').html('<a href="http://www.sfht.com/activity/794.html">亲爱的海淘会员，国庆期间因海关放假，订单发货及清关时效将会有延迟，点此了解详情</a>');
           $('.notice').fadeIn();
+        }else if (isShowOtherNotice) {
+          $('.notice .pm').html('亲爱的海淘会员，国庆期间因海关放假，订单包裹清关将会有延迟，给您带来不便，敬请谅解。');
+          $('.notice').fadeIn();
         }
+
       }
 
       this.updateCart();
