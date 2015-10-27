cc0123b03a42cb8e3b4129bdfbf8f632a483567f
zhouwenbin <330759294@qq.com>
2015-10-27 18:03:42 +0800

    同步最新代码

---
+++ app/static/html/js/slider2.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+	var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        autoplay: 5000
+    });    
+};
---
+++ app/static/html/nav.js
@@ -0,0 +1,51 @@
+$(function(){
+    //----------导航-------------//
+    $(window).scroll(function(){
+      if($(window).scrollTop() > 166){
+          $(".nav-fixed").stop(true,false).animate({
+            top:'0px',
+            opacity:1
+          },300);
+      }else{
+          $(".nav-fixed").stop(true,false).animate({
+            top:'-56px',
+            opacity:0
+          },0);
+      }
+  })
+
+  
+  //----------加入购物车-------------//
+  $('.product-1').on("click", '.btn-black', function(){
+    if($(window).scrollTop() > 166){
+        var target=$('.header-icon05').eq(1).offset()
+    }else{
+        var target=$('.header-icon05').eq(0).offset()
+    }
+    var that = $(this),
+        targetX=target.left,
+        targetY=target.top,
+        current=$(this).offset(),
+        currentX=current.left,
+        currentY=current.top,
+        cart_num=$('.cart-num-inner').eq(0).text();
+    $(this).find('.addtocart-img').eq(-1).clone().appendTo($(this));
+    $(this).find('.addtocart-img').eq(0).css({
+      left:targetX-currentX,
+      top:targetY-currentY,
+      zIndex:1000,
+      visibility:'hidden'
+    })
+    
+    setTimeout(function(){
+        that.find('.addtocart-img').eq(0).remove();
+    },1000);
+    cart_num++;
+    $('.cart-num-inner').text(cart_num);
+    $('.nav .label-error').addClass('active');
+    setTimeout(function(){
+        $('.nav .label-error').removeClass('active');
+    },500)
+    return false;
+  });
+})
