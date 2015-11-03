a09b78d7e2ad04cd9342ca200f2389cdab8660d7
zhouwenbin <330759294@qq.com>
2015-11-03 19:15:06 +0800

    购物车样式修改

--- app/static/html/slider2.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-	var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        autoplay: 5000
-    });    
-};
