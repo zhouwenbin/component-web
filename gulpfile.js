f00379b4041b1000580eafe0c250edd64d5e3a14
zhouwenbin <330759294@qq.com>
2015-11-18 15:40:14 +0800

    添加sprite

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 var gulp = require('gulp');
 var svgSprite = require("gulp-svg-sprites");
 var svg2png = require('gulp-svg2png');
+var filter = require('gulp-filter');
 
 
 gulp.task('default', function () {
     gulp.src('app/static/svg/*.svg')
         .pipe(svgSprite({mode: "symbols"}))
         .pipe(gulp.dest("app/static/inlinesvg"))
-    gulp.src('./app/static/svg/**/*.svg')
-        .pipe(svg2png())
-        .pipe(gulp.dest('./app/static/svg/png'));
+    gulp.src('app/static/svg/*.svg')
+        .pipe(svgSprite())
+        .pipe(gulp.dest("app/static/sprites")) // Write the sprite-sheet + CSS + Preview
+        .pipe(filter("**/*.svg"))  // Filter out everything except the SVG file
+        .pipe(svg2png())           // Create a PNG
+        .pipe(gulp.dest("app/static/sprites"));
 });
