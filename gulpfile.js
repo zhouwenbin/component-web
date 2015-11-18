21068858935fb9e21f0394c6a94b0b37638188e7
zhouwenbin <330759294@qq.com>
2015-11-18 15:45:51 +0800

    移动目录

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ var filter = require('gulp-filter');
 
 
 gulp.task('default', function () {
-    gulp.src('app/static/svg/*.svg')
+    gulp.src('app/static/icon/svg/*.svg')
         .pipe(svgSprite({mode: "symbols"}))
-        .pipe(gulp.dest("app/static/inlinesvg"))
-    gulp.src('app/static/svg/*.svg')
+        .pipe(gulp.dest("app/static/icon/inlinesvg"))
+    gulp.src('app/static/icon/svg/*.svg')
         .pipe(svgSprite())
-        .pipe(gulp.dest("app/static/sprites")) // Write the sprite-sheet + CSS + Preview
+        .pipe(gulp.dest("app/static/icon/sprites")) // Write the sprite-sheet + CSS + Preview
         .pipe(filter("**/*.svg"))  // Filter out everything except the SVG file
         .pipe(svg2png())           // Create a PNG
-        .pipe(gulp.dest("app/static/sprites"));
+        .pipe(gulp.dest("app/static/icon/sprites"));
 });
