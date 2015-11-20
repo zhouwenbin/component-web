55430a88d1b9ef1cf0347595c0c836bdf599d172
zhouwenbin <330759294@qq.com>
2015-11-20 15:34:42 +0800

    样式指南

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,34 @@ var gulp = require('gulp');
 var svgSprite = require("gulp-svg-sprites");
 var svg2png = require('gulp-svg2png');
 var filter = require('gulp-filter');
+var postcss = require('gulp-postcss');
 
-gulp.task('guild', function () {
-
-    var postcss = require('gulp-postcss');
+gulp.task('common', function () {
+    
     var processedCSS = fs.readFileSync('./app/static/css/pages/common/common.css', 'utf-8');
     return gulp.src('./app/static/css/pages/common/common.css')
         .pipe(postcss([
             require('postcss-style-guide')({
-                name: "样式指南",
-                processedCSS: processedCSS
+                name: "组件样式指南",
+                processedCSS: processedCSS,
+                dir: "./docs/common"
             })
         ]))
-        .pipe(gulp.dest('./docs'));
+        .pipe(gulp.dest('./docs/common'));
+});
+
+gulp.task('index2', function () {
+
+    var processedCSS = fs.readFileSync('./app/static/css/pages/index/index2.css', 'utf-8');
+    return gulp.src('./app/static/css/pages/index/index2.css')
+        .pipe(postcss([
+            require('postcss-style-guide')({
+                name: "首页样式指南",
+                processedCSS: processedCSS,
+                dir: "./docs/index2"
+            })
+        ]))
+        .pipe(gulp.dest('./docs/index2'));
 });
 
 gulp.task('default', function () {
