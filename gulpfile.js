896ee4bc140bc9dd3ba845968c46815a19c3d337
zhouwenbin <330759294@qq.com>
2015-12-02 15:19:47 +0800

    注册登录

--- gulpfile.js
+++ gulpfile.js
@@ -34,13 +34,7 @@ gulp.task('index2', function () {
 });
 
 gulp.task('default', function () {
-    gulp.src('app/static/icon/svg/*.svg')
+    gulp.src('app/static/icon/svg/login/*.svg')
         .pipe(svgSprite({mode: "symbols"}))
-        .pipe(gulp.dest("app/static/icon/inlinesvg"))
-    gulp.src('app/static/icon/svg/*.svg')
-        .pipe(svgSprite())
-        .pipe(gulp.dest("app/static/icon/sprites")) // Write the sprite-sheet + CSS + Preview
-        .pipe(filter("**/*.svg"))  // Filter out everything except the SVG file
-        .pipe(svg2png())           // Create a PNG
-        .pipe(gulp.dest("app/static/icon/sprites"));
+        .pipe(gulp.dest("app/static/icon/inlinesvg/login/"))
 });
