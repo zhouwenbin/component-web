675f5f55c257a9ac1c20ba3bc98af270d16ca2a5
zhouwenbin <330759294@qq.com>
2015-12-03 19:24:37 +0800

    替换sass和slim为postcss，模块结构调整，sag合并自动化

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,13 @@ var svgSprite = require("gulp-svg-sprites");
 var svg2png = require('gulp-svg2png');
 var filter = require('gulp-filter');
 var postcss = require('gulp-postcss');
+var include = require('gulp-html-tag-include');
+var cssnext = require("gulp-cssnext");
+var scss = require("postcss-scss");
+var module = ["login","header","footer","sidebar","index"]
 
 gulp.task('common', function () {
-    
+
     var processedCSS = fs.readFileSync('./app/static/css/pages/common/common.css', 'utf-8');
     return gulp.src('./app/static/css/pages/common/common.css')
         .pipe(postcss([
@@ -33,8 +37,43 @@ gulp.task('index2', function () {
         .pipe(gulp.dest('./docs/index2'));
 });
 
+gulp.task('sprites', function () {
+  for(var i in module){
+    gulp.src("modules/"+module[i]+"/svgs/*.svg")
+        .pipe(svgSprite({
+            common: "svg",
+            selector: module[i]+"-%f"
+        }))
+        .pipe(gulp.dest("modules/"+module[i]+"/sprites/")) // Write the sprite-sheet + CSS + Preview
+        .pipe(filter("**/*.svg"))  // Filter out everything except the SVG file
+        .pipe(svg2png())           // Create a PNG
+        .pipe(gulp.dest("modules/"+module[i]+"/sprites/"));
+  }
+
+});
+
 gulp.task('default', function () {
-    gulp.src('app/static/icon/svg/login/*.svg')
-        .pipe(svgSprite({mode: "symbols"}))
-        .pipe(gulp.dest("app/static/icon/inlinesvg/login/"))
+    //svg
+    for(var i in module){
+      gulp.src("modules/"+module[i]+"/svgs/*.svg")
+          .pipe(svgSprite({
+            mode: "symbols",
+            common: "svg",
+            selector: "icon-%f"
+          }))
+          .pipe(gulp.dest("modules/"+module[i]+"/symbols"))
+    }
+    //html
+    gulp.src('pages/user/login/posthtml/**.html')
+        .pipe(include())
+        .pipe(gulp.dest('pages/user/login/html/'));
+    //css
+    gulp.src("pages/user/login/postcss/**.css")
+    .pipe(
+        postcss([
+            require('precss')({ /* options */ })
+        ])
+    )
+    .pipe(cssnext())
+    .pipe(gulp.dest("pages/user/login/css/"))
 });
